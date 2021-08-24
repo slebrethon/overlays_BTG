8c985ce4a7b4acc41d6cafce753f507034f294ce
slebrethon <slebrethon69@hotmail.com>
2021-08-24 13:04:05 +0200

    ajustement fichier base

---
+++ js/btg_timer.js
@@ -0,0 +1,26 @@
+// timer BEHINDTHEGAME
+
+// selection DATE
+var countDownDate = new Date("Aug 21, 2021 16:30:00").getTime();
+
+// calcul TIMER
+var x = setInterval(function() {
+
+  // element TIMER
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  // var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  // var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // nomenclature TIMER
+//  document.getElementById("decounter").innerHTML = days + "" + hours + "" + minutes + ":" + seconds + "";
+  document.getElementById("decounter").innerHTML = minutes + ":" + seconds + "";
+    
+  // affichage EXPIRE 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("decounter").innerHTML = "EXPIRED";
+  }
+}, 1000);
