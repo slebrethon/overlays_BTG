13048806e92b0e53e70e03ac5553ab92db4a189d
Guillaume Deschamps <guillaumedeschamps75@gmail.com>
2022-03-31 15:44:15 +0200

    front-end for OVERLAY END

--- js/btg_variable.js
+++ js/btg_variable.js
@@ -13,11 +13,26 @@ var host_02_firstname = "aurélien"
 var host_02_lastname = "thazet"
 var host_title = "Behind The Games"
 
+var regie_01_firstname = "aurélien"
+var regie_01_lastname = "thazet"
+var regie_02_firstname = "nicolas"
+var regie_02_lastname = "vienat"
+var regie_03_firstname = "martin"
+var regie_03_lastname = "ringot"
+
+var designer_firstname = "stéphane"
+var designer_lastname = "le brethon"
+
+
 // variable PAUSE
 var pause_title = "en pause"
 var pause_subtitle = "... problème technique nous revenons dans quelques instants."
 
 // variable END
-var end_title = ""
-var end_subtitle = ""
+var end_title = "merci"
+var end_subtitle = "... pour avoir suivi notre stream."
+var end_hosts = "présenté par"
+var end_regie = "régie"
+var end_designer = "design"
+
 
